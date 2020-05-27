15441d4fd9b1467ddcc1c960a9fda0b6f2b77a89
Katerina <yekaterina.k@thoughtworks.com>
2020-05-27 14:55:38 +1000

    [Kate]fix typo

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -140,7 +140,7 @@ nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.Stati
 
 nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataFranceSaverAp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataFR" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-fr-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.tw.apps.StationLocationApp --name StationDataFranceSaverAp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=2g --conf spark.cores.max=1 /tmp/tw-raw-data-saver_2.11-0.0.1.jar kafka.${TRAINING_COHORT}.training:2181 "/tw/stationDataFR" 1>/tmp/raw-station-data-fr-saver.log 2>/tmp/raw-station-data-fr-saver.error.log &
 
 echo "====Raw Data Saver Deployed===="
 EOF
